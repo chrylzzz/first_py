80eb460320bba2605837a35b07212739d87e2773
chryl <cyl5yy412@gmail.com>
2026-01-07 15:21:19 +0800

    py内置迭代工具函数

--- day3_循环/3.三目.py
+++ day3_循环/3.三目.py
@@ -1,3 +1,6 @@
+"""
+    语法：value_if_true if condition else value_if_false
+"""
 a = 1
 b = 2
 # 条件成立 if 条件 else 条件不成立
@@ -11,3 +14,8 @@ print(c)
 # 条件成立 if 条件 else 条件不成立
 c = a if a == b else b
 print(c)
+
+# 语法：value_if_true if condition else value_if_false
+a = 10
+result = "大于5" if a > 5 else "小于等于5"
+print(result)  # 输出: 大于5
